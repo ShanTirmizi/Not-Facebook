7345881bb5df85f441613595b4b5dfe7d8bcb1a7
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2022-10-09 18:02:03 +0100

    build: added prisma new user model

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,10 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import prisma from '../../../lib/prismadb';
 
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
